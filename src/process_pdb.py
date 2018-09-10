42dbd68371f303b320297a653cb347d45ebbc2db
gabrielctn <gabrielcretin@gmail.com>
2018-09-10 23:28:19 +0200

    Rename module for PEP8

--- src/process_pdb.py
+++ src/process_pdb.py
@@ -1,9 +1,11 @@
+"""This module contains function that process PDB files to retrieve
+3D coordinates of alpha carbons and calculate the center of mass of the protein.
+"""
 
-
-def get_COM(list_coords):
+def get_com(list_coords):
     """Calculate the Center Of Mass of list of coordinates"""
     assert isinstance(
-        list_coords, list) == True, "Error 1: Input should be a list !"
+        list_coords, list), "Error 1: Input should be a list !"
     assert len(list_coords) != 0, "Error 1: The list is empty !"
     nb_coords = float(len(list_coords))
     x = 0.0
@@ -18,18 +20,17 @@ def get_COM(list_coords):
 
 def get_ca_coords(pdb_file):
     """Get the coordinates of alpha carbones in the PDB"""
-    list_CA = []
-    with open(pdb_file, 'r') as f:
-        for line in f:
+    list_ca = []
+    with open(pdb_file, 'r') as file_in:
+        for line in file_in:
             atom_type = line[0:6].strip()  # "ATOM " or "HETATM"
             atom_name = line[12:16].strip()
-            chain = line[21:22].strip()
             if atom_type == "ATOM" and atom_name == "CA":
                 residue_name = str(line[17:20].strip())
                 residue_num = int(line[22:26].strip())
                 x = float(line[30:38].strip())
                 y = float(line[38:46].strip())
                 z = float(line[46:54].strip())
-                list_CA.append({'resNum': residue_num, 'x': x,
+                list_ca.append({'resNum': residue_num, 'x': x,
                                 'y': y, 'z': z, 'resName': residue_name})
-    return list_CA
+    return list_ca
