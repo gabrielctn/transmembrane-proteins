9c2104d99d527ea98c79a3f1ca6cce24820900c8
Gabriel Cretin <gabrielcretin@gmail.com>
2018-09-14 16:25:47 +0200

    Rethought and simplified Vector Class

--- src/vector.py
+++ src/vector.py
@@ -13,43 +13,112 @@ from numbers import Number
 class Vector:
     """
     .. class:: Vector
-    This class implements 3D vectors supporting numpy arrays
+    This class implements 3D vectors (x, y, z) on top of numpy arrays
 
     Attributes:
         x: x coordinate
         y: y coordinate
         z: z coordinate
-        coords: Numpy array containing the 3D coordinates
     """
 
+    @property
+    def x(self):
+        """Gives the first element of the vector"""
+        return self._data[0]
+
+    @property
+    def y(self):
+        """Gives the first element of the vector"""
+        return self._data[1]
+
+    @property
+    def z(self):
+        """Gives the first element of the vector"""
+        return self._data[2]
+
+    @x.setter
+    def x(self, value):
+        self._data[0] = value
+
+    @y.setter
+    def y(self, value):
+        self._data[1] = value
+
+    @z.setter
+    def z(self, value):
+        self._data[2] = value
+
     def __init__(self, x=0, y=0, z=0):
-        """ Creates a vector, example: v = Vector(1, 2, 3)
-        with individual coordinates + a numpy array
+        """ Creates a vector as numpy array
+        Example: v = Vector(1, 2, 3)
         """
-        self.x = float(x)
-        self.y = float(y)
-        self.z = float(z)
-        self.coords = np.array([x, y, z])
+        self._data = np.array([x, y, z])
+
+    def __str__(self):
+        return 'Vector({0}, {1}, {2})'.format(*self._data)
+
+    def __repr__(self):
+        return 'Vector({0}, {1}, {2})'.format(*self._data)
+
+    def __mul__(self, value):
+        """Multiplication"""
+        if isinstance(value, Vector):
+            result = self._data * value._data
+        elif isinstance(value, Number):
+            result = self._data * value
+        return Vector(x=result[0], y=result[1], z=result[2])
+
+    def __rmul__(self, value):
+        """Commutative version of Multiplication"""
+        return self.__mul__(value)
+
+    def __add__(self, value):
+        """Addition"""
+        if isinstance(value, Vector):
+            result = self._data + value._data
+        elif isinstance(value, Number):
+            result = self._data + value
+        return Vector(x=result[0], y=result[1], z=result[2])
+
+    def __radd__(self, value):
+        """Commutative version of Addition"""
+        return self.__add__(value)
+
+    def __sub__(self, value):
+        """Subtraction"""
+        if isinstance(value, Vector):
+            result = self._data - value._data
+        elif isinstance(value, Number):
+            result = self._data - value
+        return Vector(x=result[0], y=result[1], z=result[2])
+
+    def __rsub__(self, value):
+        """Commutative version of Subtraction"""
+        if isinstance(value, Vector):
+            result = value._data - self._data
+        elif isinstance(value, Number):
+            result = value - self._data
+        return Vector(x=result[0], y=result[1], z=result[2])
+
+    def __neg__(self):
+        """Signing"""
+        return Vector(-self.x, -self.y, -self.z)
+
+    # Math calculations / manipulations
 
     def norm(self):
         """Calculate the norm (length, magnitude) of a vector"""
-        return np.sqrt(self.coords.dot(self.coords))  # Faster than np.linalg.norm()
+        return np.sqrt(self._data.dot(self._data))  # Faster than np.linalg.norm()
 
     def unit(self):
         """Normalize a vector by its magnitude: returns the unit vector"""
-        return np.true_divide(self.coords, self.coords.norm)
+        return np.true_divide(self, self.norm)
 
-    def vector_to(self, point):
-        """Returns the vector between self and the point given in argument"""
-        if isinstance(point, Vector):
-            return point.coords - self.coords
-        return point - self.coords
-
-    def distance_to_plane(self, normal):
+    def dist_to_plane(self, normal):
         """Calculates the distance between a 3D point and a plane
 
         Args:
-            normal: the normal vector of the plane
+            normal: The normal vector of the plane
         """
         # a plane is a*x+b*y+c*z+d=0
         # [a,b,c] is the normal. Thus, we have to calculate d
@@ -60,59 +129,3 @@ class Vector:
                                      d), np.sqrt(normal.x ** 2 +
                                                  normal.y ** 2 +
                                                  normal.z ** 2))
-
-    def create_line_between(self, dest_point):
-        pass
-
-
-    def create_far_plane(com, norm, ):
-        pass
-
-
-
-    def __str__(self):
-        """String representation"""
-        return "<{}, {}, {}>".format(self.x, self.y, self.z)
-
-    def __neg__(self):
-        """Signing"""
-        return Vector(-self.x, -self.y, -self.z)
-
-    def __mul__(self, scalar):
-        """Scalar Multiplication"""
-        return Vector(self.x * scalar,
-                      self.y * scalar,
-                      self.z * scalar)
-
-    def __div__(self, scalar):
-        """Division"""
-        return np.true_divide(self.__copy().coords, scalar)
-
-    def __add__(self, operand):
-        """Addition"""
-        return Vector(self.x + operand.x,
-                      self.y + operand.y,
-                      self.z + operand.z)
-
-    def __sub__(self, operand):
-        """Subtraction"""
-        return self.__copy() + -operand
-
-    def __getitem__(self, index):
-        if isinstance(index, int):
-            return self.coords[index]
-        # slicing
-        elif isinstance(index, type(slice(1))):
-            return self.coords.__getitem__(index)
-        else:
-            raise KeyError
-
-    def __setitem__(self, key, value):
-        if not isinstance(value, numbers.Number):
-            raise ValueError
-        if (key == 0):
-            self.x = value
-        elif (key == 1):
-            self.y = value
-        elif (key == 2):
-            self.z = value
