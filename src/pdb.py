f830f9d67c99021f696337daaf046c3402d57e7c
Gabriel Cretin <gabrielcretin@gmail.com>
2018-09-17 19:57:41 +0200

    write pdb and visualize results

--- src/pdb.py
+++ src/pdb.py
@@ -1,42 +1,61 @@
+"""This module implements functions to visualize the program's results
+with PyMol. It generates a PDB file and a .pml file."""
+
+
 from shutil import copyfile
 import os
 
 
 
-def generate_membranes_dum():
-    pass
-
-
-def write_pdb(pdb_file, plane_normal):
+def write_pdb(pdb_file, points_membrane_1, points_membrane_2):
     """ Copy the original pdb into a new file and write the coordinates of DUM
     atoms to visualize the membranes
+
+        Returns:
+            file: New PDB file with DUM atoms simulating the membranes
     """
     # Copy the original pdb file to a new pdb_file_new.pdb
     base, ext = os.path.splitext(pdb_file)
-    new_file = base + "_new" + ext
-    command = "cp " + pdb_file + " " + new_file
+    new_pdb_file = base + "_new" + ext
+    command = "cp " + pdb_file + " " + new_pdb_file
     os.system(command)
     # Write the new lines at the end of the file
-    with open(new_file, "w") as file_in:
-        line = "{:6s}{:5d} {:^4s}{:1s}{:3s} {:1s}{:4d}{:1s}   {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {:>2s}{:2s}".format("HETATM", "DUM" )
-        file_in.write()
+    with open(new_pdb_file, "a") as file_in:
+        i = 10000
+        for point in points_membrane_1:
+            if i%2 == 0:
+                line = "{:6s}{:5d}      {:3s} {:1s} {:5d}       {:8.3f} {:8.3f} {:8.3f}\n".format("HETATM ", i, "N", "DUM",i,float(point[0]), float(point[1]), float(point[2]))
+            else:
+                line = "{:6s}{:5d}      {:3s} {:1s} {:5d}       {:8.3f} {:8.3f} {:8.3f}\n".format("HETATM ", i, "O", "DUM",i,float(point[0]), float(point[1]), float(point[2]))
+            i += 1
+            file_in.write(line)
+        i = 10000
+        for point in points_membrane_2:
+            if i%2 == 0:
+                line = "{:6s}{:5d}      {:3s} {:1s} {:5d}       {:8.3f} {:8.3f} {:8.3f}\n".format("HETATM ", i, "N", "DUM",i,float(point[0]), float(point[1]), float(point[2]))
+            else:
+                line = "{:6s}{:5d}      {:3s} {:1s} {:5d}       {:8.3f} {:8.3f} {:8.3f}\n".format("HETATM ", i, "O", "DUM",i,float(point[0]), float(point[1]), float(point[2]))
+            i += 1
+            file_in.write(line)
+    return new_pdb_file
 
 
-def write_pml_script(sphere_point):
+def write_pml_script(sphere_point, pdb_file):
     """Write a small PyMol script to visualize the best line"""
     with open("src/pymol_visualize.pml", "w") as file_in:
-        line1 = "pseudoatom pt1, pos=[{}, {}, {}]".format(-sphere_point.x,
-                                                                -sphere_point.y,
-                                                                -sphere_point.z)
-        line2 = "pseudoatom pt2, pos=[{}, {}, {}]".format(sphere_point.x,
-                                                                sphere_point.y,
-                                                                sphere_point.z)
-        line3 = "distance /pt1, /pt2"
-        line4 = "set dash_gap, 0"
-        line5 = "set dash_radius, 0.3"
-        line6 = "set dash_round_ends, 0"
-        line7 = "set dash_color, 0xffcc00, dist01"
-        line8 = ("hide labels, dist01")
+        line1 = "cmd.load('{}')".format(pdb_file)
+        line2 = "cmd.pseudoatom('pt1', pos=[{}, {}, {}])".format(-sphere_point.x,
+                                                          -sphere_point.y,
+                                                          -sphere_point.z)
+        line3 = "cmd.pseudoatom('pt2', pos=[{}, {}, {}])".format(sphere_point.x,
+                                                          sphere_point.y,
+                                                          sphere_point.z)
+        line4 = "cmd.distance('/pt1', '/pt2')"
+        line5 = "cmd.set('dash_gap', '0')"
+        line6 = "cmd.set('dash_radius', '0.3')"
+        line7 = "cmd.set('dash_round_ends', '0')"
+        line8 = "cmd.set('dash_color', '0xffcc00', 'dist01')"
+        line9 = "cmd.hide('labels', 'dist01')"
         file_in.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(line1,line2,line3,
                                                               line4,line5,line6,
-                                                              line7, line8))
+                                                              line7, line8, line9))
