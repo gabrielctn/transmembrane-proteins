6f0a4f5d8e42556d8d02867ddf61b808660fb1a6
Gabriel Cretin <gabrielcretin@gmail.com>
2018-09-17 19:59:54 +0200

    Update usage and membranes generation

--- main.py
+++ main.py
@@ -4,13 +4,27 @@
 
 """
     Usage:
-        main.py FILE [--naccess PATH] [--points NUM]
+        main.py FILE [--naccess PATH] [--points NUM] [--resolution RES] [--slice SLICE]
 
     Options:
-        -h, --help                  Show this
-        -n PATH, --naccess PATH     Absolute path to local naccess binary
-        -p NUM, --points NUM        Number of points to generate on the hemisphere to criss-cross the protein.
-                                        A high number will give better results, but longer calculations. [default: 250]
+        -h, --help                   Show this
+        -n PATH, --naccess PATH      Absolute path to local naccess binary
+        -p NUM, --points NUM         Number of points to generate on the
+                                        hemisphere to criss-cross the protein.
+                                        A high number will give better results,
+                                        but longer calculations. [default: 250]
+        -r RES, --resolution RES     An integer in angströms setting the
+                                        resolution of the slicing. Setting a
+                                        resolution to 1 will create slices along
+                                        a processed line with a step of 1 angström.
+                                        A higher resolution will create less
+                                        slices of the protein, and will then
+                                        reduce the computing time [default: 5].
+        -s SLICE, --slice SLICE      An integer in angströms setting the thickness
+                                        of a slice. A thick slice will potentially
+                                        englobe more accessible residues and reduce
+                                        the number of slices. This reduces the
+                                        computations but is less resolutive. [default: 15]
 """
 
 
@@ -26,6 +40,7 @@ from multiprocessing import Pool, cpu_count
 from functools import partial
 from mpl_toolkits.mplot3d import Axes3D
 from itertools import product
+from copy import deepcopy
 
 import copy
 import math
@@ -40,7 +55,7 @@ import src.vector as vector
 import src.pdb as pdb
 
 
-def loop(processed_lines, prot_dict, sphere_point):
+def loop(processed_lines, prot_dict, thickness, resolution, sphere_point):
     """Main loop doing the calculations for 1 line of the sphere.
 
         Args:
@@ -71,20 +86,24 @@ def loop(processed_lines, prot_dict, sphere_point):
     # c_alpha - plane distances
     dist_ca_to_plane = {res_id: dist - shortest_distance for res_id, dist in dist_ca_to_plane.items()}
     # Calculate relative hydrophobicity of all c_alphas between this plane
-    # and a 15 angströms parallel plane.
+    # and a 15 angströms (by default) parallel plane.
     # The slice is sliding along the plane's normal towards the farthest
-    # c_alpha with a step of 1 angström each loop
-    nb_steps = math.ceil(longest_distance - shortest_distance)
+    # c_alpha with a step of 5 (by default) angströms each loop
+    nb_steps = math.ceil((longest_distance - shortest_distance) / resolution)
     # Dictionary containing the current processed line's informations:
     #   - Average hydrophobicity
     #   - Relative hydrophobicity in all slices
+    #   - Number of steps
+    #   - Shortest distance between residues and the plane
     line = {"slice_hydro": np.zeros((nb_steps, 1)), # The index of the array = slice_step
-            "line_average_hydro": None}
+            "line_average_hydro": None,
+            "nb_steps": nb_steps,
+            "shortest_distance": shortest_distance}
     # print("Number of slices to treat for the current line: ", nb_steps)
-    # Process all the slices of the current line
-    process_slices_of_line(dist_ca_to_plane, line, nb_steps)
-    # print(line["slice_hydro"])
-    line_average_hydro = np.true_divide(np.sum(line["slice_hydro"]), nb_steps)
+    # Process all the slices of the current line / direction
+    process_slices_of_line(dist_ca_to_plane, line, nb_steps, thickness, resolution)
+    # Calculate an average of the hydrophobicity of the line
+    line_average_hydro = np.sum(line["slice_hydro"]) * nb_steps**2
     assert isinstance(line_average_hydro, (float, int)), "Error 5: The line average hydrophobicity should be an int or float"
     # Stock the plane's normal vector and the average hydrophobicity in list as tuples:
     # Example: [ (plane_normal_1, average_hydrophobicity_1), ... ]
@@ -95,7 +114,7 @@ def loop(processed_lines, prot_dict, sphere_point):
     return processed_lines
 
 
-def process_slices_of_line(dist_ca_to_plane, line, nb_steps):
+def process_slices_of_line(dist_ca_to_plane, line, nb_steps, thickness, resolution):
     """Calculate the relative hydrophobicity of all slices of the current line.
         The slice slides along its line with a step of 1 angström
 
@@ -114,22 +133,25 @@ def process_slices_of_line(dist_ca_to_plane, line, nb_steps):
         residues_in_slice = []
         # Check which c_alphas are in the 15 angströms slice
         for res_id, dist in dist_ca_to_plane.items():
-            if 0.0 <= dist <= 15.0:
+            if 0.0 <= dist <= thickness:
                 residues_in_slice.append(prot_dict[res_id]["resName"])
         # Calculate the relative hydrophobicity of the slice
         line["slice_hydro"][step] = protein.slice_relative_hydrophobicity(residues_in_slice, len(residues_in_slice))
-        # Slide the plane along the line. Step = 1 angström
-        dist_ca_to_plane = { res_id: dist - 1 for res_id, dist in dist_ca_to_plane.items() }
+        # Slide the plane along the line. Step = 5 angströms by default
+        dist_ca_to_plane = { res_id: dist - resolution for res_id, dist in dist_ca_to_plane.items() }
     return None
 
 
 if __name__ == '__main__':
 
+    # For runtime stat
     startTime = datetime.now()
 
     # Parse command line
     arguments = docopt(__doc__, version='Transmembrane Protein Areas 1.0')
     pdb_file = arguments["FILE"]
+    thickness = int(arguments["--slice"])
+    resolution = int(arguments["--resolution"])
 
     # Run NACCESS with the Biopython wrapper
     pdb_struct = PDBParser(QUIET=True)
@@ -179,22 +201,23 @@ if __name__ == '__main__':
     # The calculations for each line of the hemisphere is parallelized.
     # This means that several lines are calculated simultaneously
     pool = Pool(processes=cpu_count())
-    func = partial(loop, processed_lines, prot_dict)
+    func = partial(loop, processed_lines, prot_dict, thickness, resolution)
     processed_lines = pool.imap(func, sphere_points)
     pool.close()
     pool.join()
-
     # Extract the "best" line, the one maximizing the average hydrophobicity
-    processed_lines = protein.get_best_slice(processed_lines)
-    best_line = protein.get_best_line(processed_lines)
+    # processed_lines = protein.get_best_slice(processed_lines)
+    best_results = protein.get_best_results(processed_lines)
 
-    print("Best line: \n\t", "Point of the sphere: ", best_line[0] + center_of_mass, "\n\tAverage hydrophobicity: {:.4f}".format(best_line[1]), "\n")
+    print("Best line: \n\t", "Point of the sphere: ", best_results[0][0] + center_of_mass, "\n\tHydrophobicity: {:.4f}".format(best_results[0][1]), "\n")
     print("Center of mass: ", center_of_mass)
 
+    # We generate the points simulating the membranes
+    pts_mb_1, pts_mb_2 = protein.generate_membranes(processed_lines, best_results, resolution)
 
-
+    new_pdb_file = pdb.write_pdb(pdb_file, pts_mb_1, pts_mb_2)
     # Write a small PyMol script to visualize the best line
-    pdb.write_pml_script(best_line[0] + center_of_mass)
+    pdb.write_pml_script(best_results[0][0] + center_of_mass, new_pdb_file)
 
     ################# RUNTIME STATS ##################################
     ##################################################################
